d1f176cdc1da7a67c1e83218ea528e29053c9fd2
Fergus <fergusfettes@gmail.com>
2017-04-07 16:08:52 +0200

    metropolis done?

--- main.cpp
+++ main.cpp
@@ -7,16 +7,17 @@ const int B = 1;
 const int L = 10;
 const int D = 2;
 
-inline short int pp(short int in) { short int out = (in + 1) % L;  return out;}
-inline short int pm(short int in) { short int out = (in + L - 1) % L;  return out;}
+inline int pp(int in) { int out = (in + 1) % L;  return out;}
+inline int pm(int in) { int out = (in + L - 1) % L;  return out;}
 
 using namespace std;
 
 class lattice {
  public:
-  lattice(int length)
+  lattice(int ll)
     {
     dimension = D;
+    length = ll;
 
     plane.resize(length);
     for(int i = 0; i < length; ++i)
@@ -26,11 +27,8 @@ class lattice {
     for(int j = 0; j < length; ++j)
         plane[i][j] = true;
 
+    p8 = exp(-8 * B);
     p4 = exp(-4 * B);
-    p2 = exp(-2 * B);
-    p0 = 1;
-    m2 = exp(2 * B);
-    m4 = exp(4 * B);
 
     }
 
@@ -43,16 +41,45 @@ class lattice {
         }
     }
 
-  void metropolis_update(int multi=1)
+  void metropolis_update()
     {
+
     double l = length; double d = dimension;
     double num = pow(l, d); int n = num;
+
     int choice = rand() % n;   //choose spin
     int r = (int)choice/length;                     //get row
     int c = choice % length;                        //and col
 
+    int nnsum1 = 0;
+    nnsum1 += plane[r][c]==plane[ r     ][ pp(c) ] ? 1 : -1;
+    nnsum1 += plane[r][c]==plane[ r     ][ pm(c) ] ? 1 : -1;
+    nnsum1 += plane[r][c]==plane[ pp(r) ][ c     ] ? 1 : -1;
+    nnsum1 += plane[r][c]==plane[ pm(r) ][ c     ] ? 1 : -1;
 
-    int nnsum += plane[r][c]==plane[r][pp(c)] ? 1 : -1;
+    plane[r][c] = !plane[r][c];                      //flip!
+
+    int nnsum2 = 0;
+    nnsum2 += plane[r][c]==plane[ r     ][ pp(c) ] ? 1 : -1;
+    nnsum2 += plane[r][c]==plane[ r     ][ pm(c) ] ? 1 : -1;
+    nnsum2 += plane[r][c]==plane[ pp(r) ][ c     ] ? 1 : -1;
+    nnsum2 += plane[r][c]==plane[ pm(r) ][ c     ] ? 1 : -1;
+
+    if (nnsum1 >= nnsum2)
+     return;
+
+    double rnd = rand() % 100; rnd /= rnd;
+    switch(nnsum2 - nnsum1)
+     {
+     case 8:
+      if (rnd < p8)
+       return;
+     case 4:
+      if (rnd < p4)
+       return;
+     }
+
+    plane[r][c] = !plane[r][c];                      //flip!
 
     }
 
@@ -60,7 +87,7 @@ class lattice {
  protected:
   vector<vector<bool> > plane;
 
-  double p4, p2, p0, m2, m4;
+  double p8, p4;
 
   int dimension, length;
 
@@ -72,6 +99,7 @@ int main()
     cout << "Hello world!" << endl;
     lattice mylat(L);
 
+    mylat.metropolis_update();
     mylat.display();
 
     return 0;
